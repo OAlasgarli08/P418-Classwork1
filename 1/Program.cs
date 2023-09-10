eb09a2116dac7836849684f86c29e72edb4a02be
Omar T. Alasgarli <omarta@code.local>
2023-09-10 14:32:34 +0400

    Finish

---
+++ 1/Program.cs
@@ -0,0 +1,59 @@
+﻿//int n = 10;
+
+//if(n % 2 == 0)
+//{
+//    Console.WriteLine("The number is even");
+//}
+//else
+//{
+//    Console.WriteLine("The number is odd");
+//}
+
+
+
+//int m = 10;
+
+//for (int i = 1; i <= m; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+//int m = 5;
+//int sum= 0;
+
+//for (int i = 0; i < m; i++)
+//{
+//    sum+=i;
+//}
+//Console.WriteLine(sum);
+
+
+
+//int m = 20;
+
+//for (int i = 0; i < m; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+
+
+
+//int m = 20;
+//int sum = 0;
+//for (int i = 0; i < m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        sum+=i;
+//    }
+//}
+//Console.WriteLine(sum);
+
+
+
